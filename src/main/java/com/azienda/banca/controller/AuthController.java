0719d9a7e3a5b6b24441f976ae34a72821a8e89b
MyUsername <samuele.maddaluno1@gmail.com>
2026-02-06 15:37:04 +0100

    feat: add dashboarController and dashboard view

--- src/main/java/com/azienda/banca/controller/AuthController.java
+++ src/main/java/com/azienda/banca/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.azienda.banca.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,6 +34,14 @@ public class AuthController {
     public String showLoginForm(){
         return "login";
     }    
+
+    @GetMapping("/dashboard")
+    public String showDashboard(Authentication authentication, Model model){
+        String username=authentication.getName();
+        model.addAttribute("username", username);
+
+        return "dashboard";
+    }
     
 
 }
