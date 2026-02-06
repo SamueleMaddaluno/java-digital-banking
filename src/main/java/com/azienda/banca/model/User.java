a3f0cbff61a49af0c40eecb2addec3a6ade35447
MyUsername <samuele.maddaluno1@gmail.com>
2026-02-06 11:29:32 +0100

    feat: add user entity

---
+++ src/main/java/com/azienda/banca/model/User.java
@@ -0,0 +1,36 @@
+package com.azienda.banca.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Table(name="users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message="Username is necessary")
+    @Size(min=4, max=20, message="Username must be beetwen 4 and 20 characters")
+    @Column(unique=true, nullable=false)
+    private String username;
+
+    @Email(message="Enter a valid email address")
+    @NotBlank(message="Email is required")
+    @Column(unique=true, nullable=false)
+    private String email;
+
+
+    private String role= "ROLE_USER";
+}
