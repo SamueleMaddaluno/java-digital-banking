0ecd424c4094d2c5808a9034378e89560c6eef18
MyUsername <samuele.maddaluno1@gmail.com>
2026-02-06 11:37:44 +0100

    feat: add UserRepository with findByUsername

---
+++ src/main/java/com/azienda/banca/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.azienda.banca.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.azienda.banca.model.User;
+
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    Optional<User>findByUsername(String username);
+}
