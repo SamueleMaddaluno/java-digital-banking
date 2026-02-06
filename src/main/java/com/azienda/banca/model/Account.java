9f373da21ab745567497fe7aab62589c2141b354
MyUsername <samuele.maddaluno1@gmail.com>
2026-02-06 18:39:33 +0100

    feat: add Account Entity

---
+++ src/main/java/com/azienda/banca/model/Account.java
@@ -0,0 +1,34 @@
+package com.azienda.banca.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="accounts")
+@Data
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique=true, nullable=false)
+    private String iban;
+
+    private BigDecimal balance =BigDecimal.ZERO;
+
+    private String accountType;
+
+    @ManyToOne
+    @JoinColumn(name="user_id",nullable=false)
+    private User user;
+}
