42fd0648b65aa15f1bb7ff41b823bab26f4d4f77
MyUsername <samuele.maddaluno1@gmail.com>
2026-02-07 13:02:32 +0100

    feat: add transaction entity

---
+++ src/main/java/com/azienda/banca/model/Transaction.java
@@ -0,0 +1,37 @@
+package com.azienda.banca.model;
+
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="transactions")
+@Data
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable=false)
+    private BigDecimal ammount;
+
+    @Column(nullable=false)
+    private String type;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name="account_id", nullable=false)
+    private Account account;
+    
+}
